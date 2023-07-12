e0aba7963d9f7b69441859fcd86557003abe6af6
falgunbokde23 <falgunbokde@gmail.com>
2023-07-12 13:00:57 +0530

    portfolio update

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ darkmode.onclick = () => {
 
 
 function contshow4(){
-       window.open(href="./documents/Falgun_ Bokde_resume.pdf"); 
+       window.open(href="./documents/Falgun_ Bokde_Resume.pdf"); 
         }
   let my_cont_4=document.querySelector(".RFAL").addEventListener("click",contshow4)
 
